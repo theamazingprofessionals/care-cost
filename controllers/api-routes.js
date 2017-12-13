00d1c07199b3bf677a09d95a7bc36488a0d83924
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-12-12 22:21:03 -0600

    tweaked and tested all the models.. still figuring out associations :|

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -3,7 +3,16 @@ const db = require("../models");
 module.exports = function (app) {
 
     app.get('/api/procedures', function (req, res) {
-        db.procedureName.findAll({}).then(function (result) {
+        db.Provider.findAll({}).then(function (result) {
+            res.json(result)
+        });
+    });
+    app.get('/api/procedures:id', function (req, res) {
+        db.Procedure.findAll({
+            where: {
+                procedureId: req.params.id
+            }
+        }).then(function (result) {
             res.json(result)
         });
     });
