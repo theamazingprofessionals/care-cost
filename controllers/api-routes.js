55caa498e2c2e77fc1e6d3c948984f70e09c3f8a
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-12-13 21:16:59 -0600

    tweaked database and added different test routes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -23,8 +23,58 @@ module.exports = function (app) {
         });
     });
 
+    app.get("/api/cost/:id/:state", function (req, res) {
+        db.Cost.findAll({
+            order: [['patientPayments', 'DESC']],
+            where: {
+                ProcedureProcedureId: req.params.id,
+            },
+            include: [{
+                model: db.Provider,
+                attributes: ['state', 'city', 'providerName'],
+                where: {
+                    state: req.params.state
+                }
 
+            }],
+
+        }).then(function (result) {
+            res.json(result);
+        });
+
+
+    })
 
 
 
 }
+
+
+
+//// examples ////
+
+
+//db.order.findAndCount({
+//    include: [{
+//        model: db.order_item,
+//        as: 'order_items',
+//        attributes: ['id', 'price', 'quantity', 'item_id'],
+//        where: orderItemConditions,
+//        include: [{
+//            model: db.item,
+//            as: 'item',
+//            attributes: ['name', 'item_group_id', 'sku'],
+//            where: itemConditions,
+//            include: [{
+//                model: db.item_group,
+//                as: 'item_group',
+//                attributes: ['name', 'item_category_id'],
+//                where: itemGroupConditions,
+//            }]
+//        }]
+//    }],
+//    order: '"order".created_at DESC',
+//    where: orderCondition,
+//    limit: 20,
+//    offset: 0
+//});
