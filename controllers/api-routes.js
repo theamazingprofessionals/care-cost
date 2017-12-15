a1624009cd9d45f378cc35e754bc0b4796825c9d
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-12-15 13:31:17 -0600

    helper and route for country min/max for given proc

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,32 +1,12 @@
 const db = require("../models");
+const helpers = require("./helpers/helpers")
 
 
+//just so i don't forget to ask someone tomorrow.. 
+//I'm not sure whats better, writing very specific routes to get more granial data using helper functions here in the controllers, or writing more general routes and then filtering as needed with our helpers on the client inside of our ajax calls??
+//seems like it'd be less repetitive on the client, but either way we can move stuff around pretty easily
 
-///this seems to be working correctly for the most part. obvi we'll need to move it into a "helpers" file or something and then require it here
 
-//**the only thing is that a few of these state averages are coming back as enomour numbers??! I'll have to double check the data tomorrow
-
-
-const stateCostAverages = queryResults => {
-    let averages = {};
-    queryResults.forEach(function (e) {
-        let state = e.Provider.state;
-        if (state in averages) {
-            averages[state].push(e.hospitalCharges);
-        } else {
-            averages[state] = [];
-            averages[state].push(e.hospitalCharges);
-        };
-    });
-    for (const state in averages) {
-        let costArray = averages[state];
-        let average = costArray.reduce((a, b) => parseFloat(a + b), 0) / costArray.length;
-        average = average.toFixed(2)
-        console.log(average)
-        averages[state] = average;
-    }
-    return averages
-};
 
 
 
@@ -35,30 +15,32 @@ const stateCostAverages = queryResults => {
 
 module.exports = function (app) {
 
-
-
-
+    //all procedures
     app.get('/api/procedures', function (req, res) {
         db.Provider.findAll({}).then(function (result) {
-            res.json(result)
+            res.json(result);
         });
     });
+    //all procedures by id
     app.get('/api/procedures:id', function (req, res) {
         db.Procedure.findAll({
             where: {
                 procedureId: req.params.id
             }
         }).then(function (result) {
-            res.json(result)
+            res.json(result);
         });
     });
 
+    //all costs
     app.get("/api/costs", function (req, res) {
         db.Cost.findAll({}).then(function (result) {
             res.json(result);
         });
     });
 
+
+    //all costs by id and state
     app.get("/api/cost/:id/:state", function (req, res) {
         db.Cost.findAll({
             order: [['hospitalCharges', 'DESC']],
@@ -71,53 +53,58 @@ module.exports = function (app) {
                 where: {
                     state: req.params.state
                 }
-
             }],
 
         }).then(function (result) {
             res.json(result);
         });
-
-
     });
 
 
-    app.get("/api/costs/all", function (req, res) {
+
+    //collect state wide average cost for a given procedure using the 'stateCostAverage' helper function
+    app.get("/api/avg/:id", function (req, res) {
         db.Cost.findAll({
-            attributes: ['hospitalCharges'],
-            order: [['hospitalCharges', 'DESC']],
+            where: {
+                ProcedureProcedureId: req.params.id,
+            },
+            attributes: ['ProcedureProcedureId', 'hospitalCharges'],
             include: [{
                 model: db.Provider,
                 attributes: ['state']
             }],
-
         }).then(function (result) {
-            res.json(stateCostAverages(result));
+            result = helpers.stateCostAverages(result)
+            res.json(result);
         });
+    });
 
 
+    app.get("/api/mm/:id", function (req, res) {
+        db.Cost.findAll({
+            where: {
+                ProcedureProcedureId: req.params.id,
+            },
+            attributes: ['ProcedureProcedureId', 'hospitalCharges'],
+            include: [{
+                model: db.Provider,
+                attributes: ['state']
+            }],
+            order: [['hospitalCharges', 'DESC']]
+        }).then(function (result) {
+            result = helpers.countryMinMax(result)
+            res.json(result)
+        })
     })
 
 
-    //returns 
-    //    [{
-    //            "hospitalCharges": "2794183.75",
-    //            "Provider": {
-    //                "state": "PA"
-    //            }
-    //},
-    //        {
-    //            "hospitalCharges": "2739156.06",
-    //            "Provider": {
-    //                "state": "CA"
-    //            }
-    //}]
+};
 
 
 
 
 
-}
+
 
 
 
