9cd29b3453156801ad49de15e7218e871cb72f3f
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-12-14 21:51:27 -0600

    added route for averaging each state.. took awhile!

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,6 +1,42 @@
 const db = require("../models");
 
+
+
+///this seems to be working correctly for the most part. obvi we'll need to move it into a "helpers" file or something and then require it here
+
+//**the only thing is that a few of these state averages are coming back as enomour numbers??! I'll have to double check the data tomorrow
+
+
+const stateCostAverages = queryResults => {
+    let averages = {};
+    queryResults.forEach(function (e) {
+        let state = e.Provider.state;
+        if (state in averages) {
+            averages[state].push(e.hospitalCharges);
+        } else {
+            averages[state] = [];
+            averages[state].push(e.hospitalCharges);
+        };
+    });
+    for (const state in averages) {
+        let costArray = averages[state];
+        let average = costArray.reduce((a, b) => parseFloat(a + b), 0) / costArray.length;
+        average = average.toFixed(2)
+        console.log(average)
+        averages[state] = average;
+    }
+    return averages
+};
+
+
+
+
+
+
 module.exports = function (app) {
+
+
+
 
     app.get('/api/procedures', function (req, res) {
         db.Provider.findAll({}).then(function (result) {
@@ -25,7 +61,7 @@ module.exports = function (app) {
 
     app.get("/api/cost/:id/:state", function (req, res) {
         db.Cost.findAll({
-            order: [['patientPayments', 'DESC']],
+            order: [['hospitalCharges', 'DESC']],
             where: {
                 ProcedureProcedureId: req.params.id,
             },
@@ -43,7 +79,41 @@ module.exports = function (app) {
         });
 
 
+    });
+
+
+    app.get("/api/costs/all", function (req, res) {
+        db.Cost.findAll({
+            attributes: ['hospitalCharges'],
+            order: [['hospitalCharges', 'DESC']],
+            include: [{
+                model: db.Provider,
+                attributes: ['state']
+            }],
+
+        }).then(function (result) {
+            res.json(stateCostAverages(result));
+        });
+
+
     })
+
+
+    //returns 
+    //    [{
+    //            "hospitalCharges": "2794183.75",
+    //            "Provider": {
+    //                "state": "PA"
+    //            }
+    //},
+    //        {
+    //            "hospitalCharges": "2739156.06",
+    //            "Provider": {
+    //                "state": "CA"
+    //            }
+    //}]
+
+
 
 
 
