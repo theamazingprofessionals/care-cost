48b0caac25aa9f0eada338944854f7f09e776325
omes000 <omes000@gmail.com>
2017-12-17 12:11:45 -0600

    some changes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,12 +1,13 @@
 var path = require("path");
 
 module.exports = function (app){
-	app.get("/", function(req, res){
-		res.sendFile(path.join(__dirname, "../public/procedures.html"));
-	})
+	// app.get("/", function(req, res){
+	// 	res.sendFile(path.join(__dirname, "../public/procedures.html"));
+	// })
 
 	app.get("/patient", function(req, res) {
-		//res.sendFile(path.join(__dirname, "../public/patientData.html"));
-		res.render("patientData");
+		//res.sendFile(path.join(__dirname, "../public/test.html"));
+		//console.log(res);
+		res.render("index");
 	})
 }
