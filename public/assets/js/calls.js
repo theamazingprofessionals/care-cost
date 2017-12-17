6c65ab5867567f783a831aa9e1ea5dad6098bfc4
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-12-17 13:02:55 -0600

    fixed ajax, more handlebars

--- public/assets/js/calls.js
+++ public/assets/js/calls.js
@@ -1,11 +1,22 @@
 $(function () {
+
+    function getRankedStateList(procId) {
+
+        $.get('/api/avg/' + procId).then(function (data) {
+            let list = "<ol>{{#each data}}<li><button data-state='{{this.state}}' data-id='{{this.procId}}'>{{this.state}}</button></li>{{/each}}</ol>";
+            let compiledTemplate = Handlebars.compile(list);
+            let html = compiledTemplate({
+                data: data
+            });
+            $("#list-div").html(html);
+        })
+    }
+
+
     $(".proc-btn").on("click", function () {
         let procId = $(this).data("id");
-        console.log(procId);
-        $.ajax('/api/avg/' + procId, {
-            type: "GET",
-        }).then(function (res) {
-            console.log(res)
-        });
+        getRankedStateList(procId)
     });
+
+
 });
