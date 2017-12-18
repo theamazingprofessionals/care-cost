5e4f9f60a02b0a40e6c8bfd24fa4c4bd7330876d
omes000 <omes000@gmail.com>
2017-12-18 15:39:51 -0600

    updating main, index, etc

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -2,14 +2,8 @@
 var path = require("path");
 
 module.exports = function (app){
-	// app.get("/", function(req, res){
-	// 	res.sendFile(path.join(__dirname, "../public/procedures.html"));
-	// })
-
 	app.get("/patient", function(req, res) {
-		//res.sendFile(path.join(__dirname, "../public/test.html"));
-		//console.log(res);
-		res.render("index");
+		res.render("patientData");
 	})
 
 	app.get("/", function (req, res) {
