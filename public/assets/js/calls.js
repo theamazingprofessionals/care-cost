db039de8754da6e2148f1cc528f8016f79f75081
omes000 <omes000@gmail.com>
2017-12-18 12:53:41 -0600

    updating for usa charts etc

--- public/assets/js/calls.js
+++ public/assets/js/calls.js
@@ -1,67 +1,93 @@
 $(function () {
-var procedureFinal;
-    function getRankedStateList(procId) {
 
-        $.get('/api/avg/' + procId).then(function (data) {
-            //console.log(data);
-            let list = "<ol>{{#each data}}<li><button class='state-select' data-state='{{this.state}}' data-id='{{this.procId}}'>{{this.state}}</button></li>{{/each}}</ol>";
-            let compiledTemplate = Handlebars.compile(list);
-            let html = compiledTemplate({
-                data: data
-            });
-            $("#list-div").empty().append(html);
-            $(".procedure-chart").attr('id', procedureFinal);
-            drawChart();
+	function getRankedStateList(procId, name) {
+
+		$.get('/api/avg/' + procId).then(function (data) {
+			//console.log(data);
+			let list = "<ol>{{#each data}}<li><button class='state-select' data-state='{{this.state}}' data-id='{{this.procId}}'>{{this.state}}</button></li>{{/each}}</ol>";
+			console.log(data);
+			getDataForMap(data, name);
+			let compiledTemplate = Handlebars.compile(list);
+			let html = compiledTemplate({
+				data: data
+			});
+			$("#list-div").empty().append(html);
+		});
+	};
+
+	function getDataForMap(data, name){
+		var mapDataArray = []; 
+		mapDataArray.push(["State", 'Average Cost of Procedure']);
+        $.each(data, function(key, value) {
+        	mapDataArray.push([value.state, value.averageCost]);
         });
-    };
-
-    function getProcedureName(procId){
-        $.get('/api/procedures/' + procId).then(function(data){
-            console.log(data);
-            var procedure = data[0].procedureName;
-            var temp = procedure.indexOf(" ");
-            if (temp !== -1){
-                var temp1 = procedure.slice(0, temp);
-                var temp2 = procedure.slice(temp+1, procedure.length);
-                procedureFinal = temp1.toLowerCase() + "_" + temp2.toLowerCase();
-            }
-            else {
-                procedureFinal = procedure.toLowerCase();
-            }
-            console.log(procedureFinal);
-        })
+        console.log(mapDataArray);
+        drawRegionsMap(mapDataArray, name);
     }
 
-    function getStateCostData(state, procId) {
-        $.get("/api/cost/" + state + "/" + procId).then(function (data) {
-            //console.log(data);
-        })
+    function getDataForState(data, name){
+    	var stateDataArray = [];
+    	stateDataArray.push(["Hospital", "Procedure Cost"]);
+    	var regionTemp = data[0].Provider.region;
+    	var region = "US-"+ regionTemp.slice(0,2);
+    	//console.log("region " + region);
+
+    	$.each(data, function(key, value){
+    		stateDataArray.push([value.Provider.address + " " + value.Provider.city + " " + value.Provider.state, value.hospitalCharges]);
+    	})
+    	console.log(stateDataArray);
+    	drawMarkersMap(stateDataArray, name, region);
     }
 
+	// function getProcedureName(procId){
+	// 	$.get('/api/procedures/' + procId).then(function(data){
+	// 		console.log(data);
+	// 		var procedure = data[0].procedureName;
+	// 		// var temp = procedure.indexOf(" ");
+	// 		// if (temp !== -1){
+	// 		// 	var temp1 = procedure.slice(0, temp);
+	// 		// 	var temp2 = procedure.slice(temp+1, procedure.length);
+	// 		// 	procedureFinal = temp1.toLowerCase() + "_" + temp2.toLowerCase();
+	// 		// }
+	// 		// else {
+	// 		// 	procedureFinal = procedure.toLowerCase();
+	// 		// }
+	// 		console.log(procedure);
+	// 	})
+	// }
 
-    $(".proc-btn").on("click", function () {
-        let procId = $(this).data("id");
-        getProcedureName(procId);
-        getRankedStateList(procId);
-    });
+	function getStateCostData(state, procId) {
+		$.get("/api/cost/" + state + "/" + procId).then(function (data) {
+			console.log(data);
+			getDataForState(data, name);
+		})
+	}
 
 
-    $(".proc-btn").hover(function () {
-        let id = $(this).data("id");
-        let name = $(this).data("name");
-        $.get("/api/mm/" + id).then(function (data) {
-            console.log(data);
-            console.log(data[0])
-            minMaxChart(name, data[0].state, data[0].min, data[1].state, data[1].max)
-        })
-    })
+	$(".proc-btn").on("click", function () {
+		let procId = $(this).data("id");
+		let name = $(this).text();
+		getRankedStateList(procId, name);
+	});
 
-    $(document).on("click", ".state-select", function () {
-        console.log("clicked")
-        let state = $(this).data("state").toLowerCase();
-        let procId = $(this).data("id");
-        getStateCostData(state, procId);
-    })
+
+	$(".proc-btn").hover(function () {
+		let id = $(this).data("id");
+		let name = $(this).data("name");
+		$.get("/api/mm/" + id).then(function (data) {
+			console.log(data);
+			console.log(data[0])
+			minMaxChart(name, data[0].state, data[0].min, data[1].state, data[1].max)
+		})
+	})
+
+	$(document).on("click", ".state-select", function () {
+		console.log("clicked")
+		let state = $(this).data("state").toLowerCase();
+		let procId = $(this).data("id");
+		//getProcedureName(procId);
+		getStateCostData(state, procId);
+	})
 
 
 
--- public/assets/js/min-max.js
+++ public/assets/js/min-max.js
@@ -42,5 +42,43 @@ function minMaxChart(procedure, state1, num1, state2, num2) {
     chart.draw(data, google.charts.Bar.convertOptions(options));
 };
 
+google.charts.load('current', {
+    'packages':['geochart'],
+    'mapsApiKey': 'AIzaSyCfZ7tXLzQQU_yWm7iJJwWjKwaasAFUUBY'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap(dataArray, name) {
+    var data = google.visualization.arrayToDataTable(dataArray);
+
+    var options = {
+        region: 'US',
+        displayMode: 'regions',
+        resolution: 'provinces',
+        colorAxis: {colors: ['#dee1e5', '#4374e0']} // gray to blue
+    };
+    var chart = new google.visualization.GeoChart(document.getElementById('map'));
+    chart.draw(data, options);
+};
+
+google.charts.load('current', {
+    'packages':['geochart'],
+    'mapsApiKey': 'AIzaSyCfZ7tXLzQQU_yWm7iJJwWjKwaasAFUUBY'
+});
+google.charts.setOnLoadCallback(drawMarkersMap);
+
+function drawMarkersMap(dataArray, name, region) {
+    var data = google.visualization.arrayToDataTable(dataArray);
+
+    var options = {
+        sizeAxis: {minValue: 0, maxValue: 100},
+        displayMode: 'markers',
+        region: region,
+        resolution: 'provinces',
+        colorAxis: {colors: ['#dee1e5', '#4374e0']} // gray to blue
+    };
+    var chart = new google.visualization.GeoChart(document.getElementById('state_map'));
+    chart.draw(data, options);
+};
 
 $(document).ready(minMaxChart("Heart Transplant", "Pennsylvania", 2794182, "Arizona", 382051))
